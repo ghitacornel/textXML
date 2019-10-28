779b3072cca20fac4d0279d3e951dccebd083db1
ghitacornel <ghita.cornel@gmail.com>
2019-10-28 16:30:24 +0200

    no format

--- testJAXP/src/test/java/tests/TestJAXP.java
+++ testJAXP/src/test/java/tests/TestJAXP.java
@@ -8,7 +8,30 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import utils.Utils;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.nio.file.Paths;
+
 public class TestJAXP {
+
+    @Test
+    public void validate_XML_With_XSD() {
+        boolean valid;
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        try {
+            Schema schema = schemaFactory.newSchema(Paths.get("src", "test", "resources", "input_students.xsd").toFile());
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(Paths.get("src", "test", "resources", "input_students.xml").toFile()));
+            valid = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            valid = false;
+        }
+        Assert.assertTrue(valid);
+    }
 
     @Test
     public void parseValidateAndChangeXML() {
